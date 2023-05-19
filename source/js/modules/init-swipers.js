8f61d256047243471906cef2bddb2c19380dfdfa
alex-v-10 <alex.ru9311@gmail.com>
2023-05-20 02:18:39 +0300

    fix bugs

--- source/js/modules/init-swipers.js
+++ source/js/modules/init-swipers.js
@@ -1,31 +1,47 @@
 import Swiper from '../vendor/swiper';
 
 export const initSwipers = () => {
-  (() => {
-    return new Swiper('.trainers__swiper', {
-      direction: 'horizontal',
-      loop: true,
-      navigation: {
-        nextEl: '.trainers__next',
-        prevEl: '.trainers__prev',
+  const trainersSwiper = new Swiper('.trainers__swiper', {
+    direction: 'horizontal',
+    loop: true,
+    navigation: {
+      nextEl: '.trainers__next',
+      prevEl: '.trainers__prev',
+    },
+    spaceBetween: 40,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 40,
       },
-      spaceBetween: 40,
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 40,
-        },
-        767: {
-          slidesPerView: 2,
-          spaceBetween: 30,
-        },
-        1199: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
       },
-    });
-  })();
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
+
+  const trainerCards = document.querySelectorAll('.swiper-slide:not(.swiper-slide-duplicate) .trainer-card');
+  if (trainerCards.length) {
+    for (const card of trainerCards) {
+      card.setAttribute('tabindex', '0');
+    }
+  }
+
+  const onBreakpointChange = () => {
+    const duplicates = document.querySelectorAll('.swiper-slide-duplicate .trainer-card');
+    if (duplicates.length) {
+      for (const card of duplicates) {
+        card.setAttribute('tabindex', '-1');
+      }
+    }
+  };
+
+  trainersSwiper.on('breakpoint', onBreakpointChange);
 
   (() => {
     return new Swiper('.reviews__swiper', {
